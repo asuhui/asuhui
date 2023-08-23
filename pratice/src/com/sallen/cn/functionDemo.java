3384c29ddb75bc112131ce3416f163f8b54929f0
Sallen_sha <18726003966@163.com>
2023-08-23 23:46:54 +0800

    foreach

--- pratice/src/com/sallen/cn/functionDemo.java
+++ pratice/src/com/sallen/cn/functionDemo.java
@@ -9,6 +9,7 @@ public class functionDemo {
         fun3();
         fun3(1, 2, 3);
         fun3(new int[]{3, 4, 5});
+        fun4(1, 2, 3);
     }
 
     //方法的重载只有参数有关不与返回值有关
@@ -23,4 +24,11 @@ public class functionDemo {
     public static void fun3(int... a) {//不得长参数
         System.out.println(Arrays.toString(a));
     }
+
+    public static void fun4(int... arr) {//不得长参数
+        for (int i : arr) {//foreach iter
+            System.out.println(i);
+        }
+    }
+
 }
