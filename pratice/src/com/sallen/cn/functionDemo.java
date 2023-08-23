6935871bb89e6341124b56bb9d0d8f591dd49021
Sallen_sha <18726003966@163.com>
2023-08-23 23:39:47 +0800

    function reload and not sure parameters

---
+++ pratice/src/com/sallen/cn/functionDemo.java
@@ -0,0 +1,26 @@
+package com.sallen.cn;
+
+import java.util.Arrays;
+
+public class functionDemo {
+    public static void main(String[] args) {
+        fun1(1);
+        fun1(2, 3);
+        fun3();
+        fun3(1, 2, 3);
+        fun3(new int[]{3, 4, 5});
+    }
+
+    //方法的重载只有参数有关不与返回值有关
+    public static void fun1(int a) {
+        System.out.println(a);
+    }
+
+    public static void fun1(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void fun3(int... a) {//不得长参数
+        System.out.println(Arrays.toString(a));
+    }
+}
