e9f3607c47d395f4e5a24b7e73866e066e7e439b
Sallen_sha <18726003966@163.com>
2023-10-08 16:22:33 +0800

    Generic demo

---
+++ pratice/src/com/most/docker/GenericsDemo.java
@@ -0,0 +1,25 @@
+package com.most.docker;
+
+import java.util.*;
+
+public class GenericsDemo {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("hello");
+        list.add("world");
+
+        Iterator<String> iter = list.iterator();
+
+        while (iter.hasNext()) {
+            String s = iter.next();
+            System.out.println(s);
+        }
+
+        Map<String, String> map = new HashMap<>();
+        map.put("sha", "sxz");
+        map.put("zhu", "zxt");
+
+        String ss = map.get("sha");
+        System.out.println(ss);
+    }
+}
